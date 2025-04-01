01b11a1f6b62488596523969b53d14ca6f6b1fea
jwon0523 <jwon010523@gmail.com>
2025-04-02 01:25:47 +0900

    ✨Add SwiftUI ScrollView example

---
+++ SwiftUI/ScrollviewExample/ScrollviewExample/ScrollviewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollviewExampleApp.swift
+//  ScrollviewExample
+//
+//  Created by jaewon Lee on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollviewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
