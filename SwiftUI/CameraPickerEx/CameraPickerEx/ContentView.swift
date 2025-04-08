2fa7acd5ab85489ef790f53f6b25158054dbaec9
jwon0523 <jwon010523@gmail.com>
2025-04-08 21:39:41 +0900

    ✨ActionSheet 활용하여 카메라 촬영 혹은 앨범에서 이미지 가져오기

--- SwiftUI/CameraPickerEx/CameraPickerEx/ContentView.swift
+++ SwiftUI/CameraPickerEx/CameraPickerEx/ContentView.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    var body: some View {
+        CameraPickerWithActionSheet()
+    }
+}
+
+struct CameraPickerView: View {
     @State private var showCamera = false
     @State private var capturedImage: UIImage?
     
@@ -33,6 +40,79 @@ struct ContentView: View {
             }
         }
     }
+}
+
+struct CameraPickerWithActionSheet: View {
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var images: [UIImage] = []
+    
+    @State private var showCamera = false
+    @State private var showActionSheet = false
+    @State private var showPhotosPicker = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Button("사진 추가하기") {
+                showActionSheet = true
+            }
+            
+            // View로 불러오는 방법인듯?
+//            PhotosPicker(
+//                "사진 선택하기",
+//                selection: $selectedItems,
+//                maxSelectionCount: 5,
+//                matching: .images
+//            )
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(images, id:\.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                    }
+                }
+            }
+        }
+        .padding()
+        .confirmationDialog(
+            "사진을 어떻게 추가할까요?",
+            isPresented: $showActionSheet
+        ) {
+            Button("앨범에서 가져오기") {
+                showPhotosPicker = true
+            }
+            
+            Button("카메라로 촬영하기") {
+                showCamera = true
+            }
+            
+            Button("취소", role: .cancel) {}
+        }
+        .sheet(isPresented: $showCamera) {
+            CameraPicker { image in
+                images.append(image)
+            }
+        }
+        .photosPicker(
+            isPresented: $showPhotosPicker,
+            selection: $selectedItems,
+            maxSelectionCount: 3,
+            matching: .images
+        )
+        .onChange(of: selectedItems) { oldValue, newValue in
+            for item in newValue {
+                Task {
+                    if let data = try? await item.loadTransferable(type: Data.self),
+                       let image = UIImage(data: data) {
+                        images.append(image)
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 #Preview {
