36a83b679f5899e89f26f9e358bfe7d27a2fdf7b
jwon0523 <jwon010523@gmail.com>
2025-04-28 15:02:10 +0900

    feat: LocationManager 구현

--- SwiftUI/MapkitEx/MapkitEx/ContentView.swift
+++ SwiftUI/MapkitEx/MapkitEx/ContentView.swift
@@ -6,16 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780),
+        // MKCoordinateSpan - 지도에서 가로/세로 방향으로 얼마나 확대해서 보여줄지를 정하는 값. 작을수록 확돼됨.
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(initialPosition: .region(region))
     }
 }
 
---
+++ SwiftUI/MapkitEx/MapkitEx/LocationManager.swift
@@ -0,0 +1,162 @@
+//
+//  LocationManager.swift
+//  MapkitEx
+//
+//  Created by jaewon Lee on 4/28/25.
+//
+
+import CoreLocation
+import MapKit
+
+@Observable
+class LocationManager: NSObject {
+    static let shared = LocationManager()
+    
+    private let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation?
+    var currentHeading: CLHeading?
+    
+    var currentSpeed: CLLocationSpeed = 0
+    var currentDirection: CLLocationDirection = 0
+    var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    var didEnterGeofence: Bool = false
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        /// kCLLocationAccuracyBest 설정
+        /// - 가능한 가장 높은 수준의 정확도로 위치를 요청.
+        /// - GPS, Wi-Fi, 셀룰러, 블루투스 등 모든 가용 수단을 활용하여 최대한 정확한 위치를 가져옴.
+        /// - 특히 내비게이션이나 지도 중심 앱에서 유용.
+        /// - 단점: 배터리 소모가 크고, 리소스 사용량이 높을 수 있다.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        /// kCLHeadingFilterNone
+        /// - 방향(나침반) 업데이트 민감도 설정
+        /// - 아주 작은 변화도 모두 반영
+        /// - 사용자가 핸드폰을 살짝 돌려도 방향 정보가 업데이트 됨
+        locationManager.headingFilter = kCLHeadingFilterNone
+    }
+    
+    // MARK: - 권한 요청
+    func requestAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    // MARK: - 위치 추적
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: - 방향 추적
+    func startUpdatingHeading() {
+        locationManager.startUpdatingHeading()
+    }
+    
+    func stopUpdatingHeading() {
+        locationManager.stopUpdatingHeading()
+    }
+    
+    // MARK: - Significant Location Change
+    func startMonitoringSignificantLocationChanges() {
+        locationManager.startMonitoringSignificantLocationChanges()
+    }
+    
+    func stopMonitoringSignificantLocationChanges() {
+        locationManager.stopMonitoringSignificantLocationChanges()
+    }
+    
+    // MARK: - 방문 감지
+    func startMonitoringVisits() {
+        locationManager.startMonitoringVisits()
+    }
+    
+    // MARK: - 지오펜싱
+    func startMonitoringGeofence(
+        center: CLLocationCoordinate2D,
+        radius: CLLocationDistance,
+        identifier: String
+    ) async {
+        let region = CLCircularRegion(
+            center: center,
+            radius: radius,
+            identifier: identifier
+        )
+        
+        region.notifyOnEntry = true
+        region.notifyOnExit = true
+        
+        // FIXME: Deprecated 코드이므로 변경 필요
+        locationManager.startMonitoring(for: region)
+        
+//        let monitor = await CLMonitor("MapKitExMonitor")
+//        
+//        let condition = CLMonitor.CircularGeographicCondition(center: center, radius: radius)
+//        
+//        await monitor.add(condition, identifier: identifier)
+    }
+    
+    func stopMonitoringAllGeofences() {
+        for region in locationManager.monitoredRegions {
+            // FIXME: Deprecated 코드이므로 변경 필요
+            locationManager.stopMonitoring(for: region)
+        }
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    // 권한 변경 감지
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    // 위치 업데이트 감지(기본 위치 추적 + Significant Change)
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        guard let latest = locations.last else { return }
+        
+        DispatchQueue.main.async {
+            self.currentLocation = latest
+            self.currentSpeed = max(latest.speed, 0)
+        }
+    }
+    
+    // 방향 감지
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        DispatchQueue.main.async {
+            self.currentHeading = newHeading
+            self.currentDirection = newHeading.trueHeading > 0
+            ? newHeading.trueHeading
+            : newHeading.magneticHeading
+        }
+    }
+    
+    // 방문 감지 (visit monitoring)
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        print("방문 감지됨 - 좌표: \(visit.coordinate), 도착: \(visit.arrivalDate), 출발: \(visit.departureDate)")
+    }
+    
+    // 지오펜싱: 진입
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        DispatchQueue.main.async {
+            self.didEnterGeofence = true
+        }
+    }
+    
+    // 지오펜싱: 이탈
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        DispatchQueue.main.async {
+            self.didEnterGeofence = false
+        }
+    }
+    
+    // 오류 처리
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("위치 오류: \(error.localizedDescription)")
+    }
+}
