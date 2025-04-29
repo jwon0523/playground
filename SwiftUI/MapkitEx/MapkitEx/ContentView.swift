94930d9e66ff29865925c9a80145602914dec3c8
jwon0523 <jwon010523@gmail.com>
2025-04-29 14:44:36 +0900

    feat: SearchMapView done

--- SwiftUI/MapkitEx/MapkitEx/ContentView.swift
+++ SwiftUI/MapkitEx/MapkitEx/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    var body: some View {
+        SearchMapView()
+    }
     
+}
+
+fileprivate struct MapView: View {
+    // MARK: - 객체 선언
     @State private var motionManager = MotionManager.shared
     @State private var locationManager = LocationManager.shared
     @State private var viewModel: MapViewModel = .init()
     
+    // MARK: - 상태 선언
     @State private var lastKnownLocation: CLLocationCoordinate2D?
     @State private var showSearchButton: Bool = false
-    
-    @Namespace var mapScope
-    
     @State private var showEnteredAlert: Bool = false
     @State private var showExitedAlert: Bool = false
+    
+    @Namespace var mapScope
     
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
@@ -114,6 +121,59 @@ struct ContentView: View {
                 }
             })
         })
+    }
+}
+
+fileprivate struct SearchMapView: View {
+    @State private var viewModel: MapViewModel = .init()
+    @State private var locationManager: LocationManager = .shared
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("장소를 검색하세요", text: $searchText) {
+                if let location = locationManager.currentLocation {
+                    viewModel.search(query: searchText, to: location)
+                }
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            
+            Map(
+                position: $viewModel.cameraPosition,
+                bounds: .none,
+                interactionModes: .all
+            ) {
+                ForEach(viewModel.searchResults, id: \.id) { place in
+                    if let coordinate = place.mapItem.placemark.location?.coordinate {
+                        Annotation(
+                            place.mapItem.name ?? "이름없음",
+                            coordinate: coordinate
+                        ) {
+                            Image(systemName: "mappin")
+                                .renderingMode(.template)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(Color.red)
+                        }
+                    }
+                }
+            }
+            .frame(height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+            
+            List(viewModel.searchResults) { place in
+                VStack(alignment: .leading) {
+                    Text(place.mapItem.name ?? "이름 없음")
+                        .font(.headline)
+                    Text(place.mapItem.placemark.title ?? "")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
     }
 }
 
--- SwiftUI/MapkitEx/MapkitEx/MapViewModel.swift
+++ SwiftUI/MapkitEx/MapkitEx/MapViewModel.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 import MapKit
 
+struct Place: Identifiable, Hashable {
+    let id = UUID()
+    
+    /// 'MKMapItem 타입 내부 속정
+    /// - .name: 장소이름
+    /// - .placemark.coordinate: 위도/경도
+    /// - .placemark.title: 주소 전체
+    /// - .phoneNumber, .url: 전화번호, 웹사이트 등
+    let mapItem: MKMapItem
+}
+
 @Observable
 final class MapViewModel {
     var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     var currentMapCenter: CLLocationCoordinate2D?
+    
+    var searchResults: [Place] = []
+    
+    /// `MKCoordinateRegion`: 지도에서 보여줄 여역(중심 좌표 + 줌 정도), 카메로 위치 설정에 사용
+    var region: MKCoordinateRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.583123, longitude: 127.010695),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
     
     var makers: [Marker] = [
         .init(
@@ -34,5 +53,26 @@ final class MapViewModel {
             radius: geofenceRadius,
             identifier: geofenceIdentifier
         )
+    }
+    
+    func search(query: String, to location: CLLocation) {
+        // MKLocalSearch - 실제로 검색을 실행하는 객체
+        // 어떤 키워드로, 어떤 위치 기준으로 검색할지 설정
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        request.region = .init(
+            center: location.coordinate,
+            span: .init(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+        
+        let search = MKLocalSearch(request: request)
+        // 비동기로 검색을 실행, 결과를 클로저로 반환
+        search.start { [weak self] response, error in
+            guard let self, let mapItems = response?.mapItems else { return }
+            
+            DispatchQueue.main.async {
+                self.searchResults = mapItems.map { Place(mapItem: $0) }
+            }
+        }
     }
 }
