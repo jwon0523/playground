4bacb2a6630b903f16b6baf96e7a4e0d4c32b681
jwon0523 <jwon010523@gmail.com>
2025-04-29 20:06:10 +0900

    feat: HybridMapView done

--- SwiftUI/MapkitEx/MapkitEx/ContentView.swift
+++ SwiftUI/MapkitEx/MapkitEx/ContentView.swift
@@ -10,7 +10,7 @@ import MapKit
 
 struct ContentView: View {
     var body: some View {
-        MapView()
+        HybridMapContainerView()
     }
 }
 
--- SwiftUI/MapkitEx/MapkitEx/View/Hybrid/HybridMapView.swift
+++ SwiftUI/MapkitEx/MapkitEx/View/Hybrid/HybridMapView.swift
@@ -6,19 +6,66 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct HybridMapView: View {
+struct HybridMapView: UIViewRepresentable {
     
-    @Bindable private var locationManager: LocationManager = .shared
+    @Binding var selectedCoordinate: CLLocationCoordinate2D?
+    @Binding var cameraCenter: CLLocationCoordinate2D?
     
-    var body: some View {
-        ZStack {
-            CustomMap(locationManager: locationManager)
-                .ignoresSafeArea(.all)
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .none
+        
+        // MARK: LongPressGesture
+        let gesture = UILongPressGestureRecognizer(
+            target: context.coordinator,
+            action: #selector(context.coordinator.handleLongPress(_:))
+        )
+        mapView.addGestureRecognizer(gesture)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        if let coordinate = selectedCoordinate {
+            uiView.removeAnnotations(uiView.annotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "선택한 위치"
+            uiView.addAnnotation(annotation)
+        }
+        
+        cameraCenter = uiView.centerCoordinate
+    }
+    
+    
+    // MARK: - Nested Type의 Coordinator 클래스
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: HybridMapView
+        
+        init(_ parent: HybridMapView) {
+            self.parent = parent
+        }
+        
+        @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+            guard gestureRecognizer.state == .began,
+                  let mapView = gestureRecognizer.view as? MKMapView else { return }
+            
+            let point = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+            parent.selectedCoordinate = coordinate
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.cameraCenter = mapView.centerCoordinate
         }
     }
-}
-
-#Preview {
-    HybridMapView()
 }
